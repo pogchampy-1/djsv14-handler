0c3231dc08fa3f9ce42fcfa09da08301a4fe1eea
pogchampy-1 <robertskian40@gmail.com>
2022-07-23 10:03:57 +0200

    incorrect params

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,11 +1,11 @@
-const { Client, ChatInputCommandInteraction } = require("discord.js");
+const { Client, Interaction } = require("discord.js");
 
 module.exports = {
     data: {
         name: "interactionCreate",
         once: false,
     },
-    /** @param {Client} client @param {ChatInputCommandInteraction} interaction */
+    /** @param {Client} client @param {Interaction} interaction */
     execute: async (client, interaction) => {
         if (interaction.isChatInputCommand()) {
             const command = client.commands.get(interaction.commandName);
